5f55c4dd1e14797779781fcee3b3faaac6a0e358
vozerov-gridgain <vozerov@gridgain.com>
2016-04-27 10:46:36 +0300

    Moved near data to extras.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridNearAtomicSingleUpdateResponse.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/atomic/GridNearAtomicSingleUpdateResponse.java
@@ -70,24 +70,8 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
     @GridToStringInclude
     private boolean remapKey;
 
-    /** Indexes of keys for which values were generated on primary node (used if originating node has near cache). */
-    private boolean nearValsIdx;
-
-    /** Indexes of keys for which update was skipped (used if originating node has near cache). */
-    private boolean nearSkipIdx;
-
-    /** Values generated on primary node which should be put to originating node's near cache. */
-    @GridToStringInclude
-    private CacheObject nearVal;
-
-    /** Version generated on primary node to be used for originating node's near cache update. */
-    private GridCacheVersion nearVer;
-
-    /** Near TTLs. */
-    private long nearTtl = -1L;
-
-    /** Near expire times. */
-    private long nearExpireTime = -1L;
+    /** Near extras. */
+    private NearExtras nearExtras;
 
     /**
      * Empty constructor required by {@link Externalizable}.
@@ -186,10 +170,12 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
     @Override public void addNearValue(int keyIdx, @Nullable CacheObject val, long ttl, long expireTime) {
         assert keyIdx == 0;
 
+        initializeNearExtras();
+
         addNearTtl(keyIdx, ttl, expireTime);
 
-        nearValsIdx = true;
-        nearVal = val;
+        nearExtras.valsIdx = true;
+        nearExtras.val = val;
     }
 
     /** {@inheritDoc} */
@@ -197,39 +183,45 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
     @Override public void addNearTtl(int keyIdx, long ttl, long expireTime) {
         assert keyIdx == 0;
 
-        nearTtl = ttl;
-        nearExpireTime = expireTime;
+        initializeNearExtras();
+
+        nearExtras.ttl = ttl;
+        nearExtras.expireTime = expireTime;
     }
 
     /** {@inheritDoc} */
     @Override public long nearExpireTime(int idx) {
         assert idx == 0;
 
-        return nearExpireTime;
+        return nearExtras != null ? nearExtras.expireTime : -1;
     }
 
     /** {@inheritDoc} */
     @Override public long nearTtl(int idx) {
         assert idx == 0;
 
-        return nearTtl;
+        return nearExtras != null ? nearExtras.ttl : -1;
     }
 
     /** {@inheritDoc} */
     @Override public void nearVersion(GridCacheVersion nearVer) {
-        this.nearVer = nearVer;
+        initializeNearExtras();
+
+        nearExtras.ver = nearVer;
     }
 
     /** {@inheritDoc} */
     @Override public GridCacheVersion nearVersion() {
-        return nearVer;
+        return nearExtras != null ? nearExtras.ver : null;
     }
 
     /** {@inheritDoc} */
     @Override public void addSkippedIndex(int keyIdx) {
         assert keyIdx == 0;
 
-        nearSkipIdx = true;
+        initializeNearExtras();
+
+        nearExtras.skipIdx = true;
 
         addNearTtl(keyIdx, -1L, -1L);
     }
@@ -238,21 +230,21 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
     @Override public boolean isNearSkippedIndex(int idx) {
         assert idx == 0;
 
-        return nearSkipIdx;
+        return nearExtras != null && nearExtras.skipIdx;
     }
 
     /** {@inheritDoc} */
     @Override public boolean isNearValueIndex(int idx) {
         assert idx == 0;
 
-        return nearValsIdx;
+        return nearExtras != null && nearExtras.valsIdx;
     }
 
     /** {@inheritDoc} */
     @Override @Nullable public CacheObject nearValue(int idx) {
         assert idx == 0;
 
-        return nearVal;
+        return nearExtras != null ? nearExtras.val : null;
     }
 
     /** {@inheritDoc} */
@@ -299,6 +291,14 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
         return true;
     }
 
+    /**
+     * Initialize near extras.
+     */
+    private void initializeNearExtras() {
+        if (nearExtras == null)
+            nearExtras = new NearExtras();
+    }
+
     /** {@inheritDoc}
      * @param ctx*/
     @Override public void prepareMarshal(GridCacheSharedContext ctx) throws IgniteCheckedException {
@@ -309,7 +309,8 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
 
         GridCacheContext cctx = ctx.cacheContext(cacheId);
 
-        prepareMarshalCacheObject(nearVal, cctx);
+        if (nearExtras != null)
+            prepareMarshalCacheObject(nearExtras.val, cctx);
 
         if (ret != null)
             ret.prepareMarshal(cctx);
@@ -324,13 +325,12 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
 
         GridCacheContext cctx = ctx.cacheContext(cacheId);
 
-        finishUnmarshalCacheObject(nearVal, cctx, ldr);
+        if (nearExtras != null)
+            finishUnmarshalCacheObject(nearExtras.val, cctx, ldr);
 
         if (ret != null)
             ret.finishUnmarshal(cctx, ldr);
     }
-
-
 
     /** {@inheritDoc} */
     @Override public boolean writeTo(ByteBuffer buf, MessageWriter writer) {
@@ -366,53 +366,61 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
                 writer.incrementState();
 
             case 6:
-                if (!writer.writeLong("nearExpireTime", nearExpireTime))
-                    return false;
-
-                writer.incrementState();
-
-            case 7:
-                if (!writer.writeBoolean("nearSkipIdx", nearSkipIdx))
-                    return false;
-
-                writer.incrementState();
-
-            case 8:
-                if (!writer.writeLong("nearTtl", nearTtl))
-                    return false;
-
-                writer.incrementState();
-
-            case 9:
-                if (!writer.writeMessage("nearVal", nearVal))
-                    return false;
-
-                writer.incrementState();
-
-            case 10:
-                if (!writer.writeBoolean("nearValsIdx", nearValsIdx))
-                    return false;
-
-                writer.incrementState();
-
-            case 11:
-                if (!writer.writeMessage("nearVer", nearVer))
-                    return false;
-
-                writer.incrementState();
-
-            case 12:
                 if (!writer.writeBoolean("remapKey", remapKey))
                     return false;
 
                 writer.incrementState();
 
-            case 13:
+            case 7:
                 if (!writer.writeMessage("ret", ret))
                     return false;
 
                 writer.incrementState();
 
+            case 8:
+                if (!writer.writeBoolean("hasNearExtras", nearExtras != null))
+                    return false;
+
+                writer.incrementState();
+
+                if (nearExtras == null)
+                    return true;
+
+            case 9:
+                if (!writer.writeLong("nearExpireTime", nearExtras.expireTime))
+                    return false;
+
+                writer.incrementState();
+
+            case 10:
+                if (!writer.writeBoolean("nearSkipIdx", nearExtras.skipIdx))
+                    return false;
+
+                writer.incrementState();
+
+            case 11:
+                if (!writer.writeLong("nearTtl", nearExtras.ttl))
+                    return false;
+
+                writer.incrementState();
+
+            case 12:
+                if (!writer.writeMessage("nearVal", nearExtras.val))
+                    return false;
+
+                writer.incrementState();
+
+            case 13:
+                if (!writer.writeBoolean("nearValsIdx", nearExtras.valsIdx))
+                    return false;
+
+                writer.incrementState();
+
+            case 14:
+                if (!writer.writeMessage("nearVer", nearExtras.ver))
+                    return false;
+
+                writer.incrementState();
         }
 
         return true;
@@ -454,54 +462,6 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
                 reader.incrementState();
 
             case 6:
-                nearExpireTime = reader.readLong("nearExpireTime");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 7:
-                nearSkipIdx = reader.readBoolean("nearSkipIdx");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 8:
-                nearTtl = reader.readLong("nearTtl");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 9:
-                nearVal = reader.readMessage("nearVal");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 10:
-                nearValsIdx = reader.readBoolean("nearValsIdx");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 11:
-                nearVer = reader.readMessage("nearVer");
-
-                if (!reader.isLastRead())
-                    return false;
-
-                reader.incrementState();
-
-            case 12:
                 remapKey = reader.readBoolean("remapKey");
 
                 if (!reader.isLastRead())
@@ -509,7 +469,7 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
 
                 reader.incrementState();
 
-            case 13:
+            case 7:
                 ret = reader.readMessage("ret");
 
                 if (!reader.isLastRead())
@@ -517,6 +477,67 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
 
                 reader.incrementState();
 
+            case 8:
+                boolean hasNearExtras = reader.readBoolean("hasNearExtras");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                if (hasNearExtras)
+                    nearExtras = new NearExtras();
+
+                reader.incrementState();
+
+                if (!hasNearExtras)
+                    return reader.afterMessageRead(GridNearAtomicSingleUpdateResponse.class);
+
+            case 9:
+                nearExtras.expireTime = reader.readLong("nearExpireTime");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
+
+            case 10:
+                nearExtras.skipIdx = reader.readBoolean("nearSkipIdx");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
+
+            case 11:
+                nearExtras.ttl = reader.readLong("nearTtl");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
+
+            case 12:
+                nearExtras.val = reader.readMessage("nearVal");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
+
+            case 13:
+                nearExtras.valsIdx = reader.readBoolean("nearValsIdx");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
+
+            case 14:
+                nearExtras.ver = reader.readMessage("nearVer");
+
+                if (!reader.isLastRead())
+                    return false;
+
+                reader.incrementState();
         }
 
         return reader.afterMessageRead(GridNearAtomicSingleUpdateResponse.class);
@@ -529,11 +550,35 @@ public class GridNearAtomicSingleUpdateResponse extends GridNearAtomicAbstractUp
 
     /** {@inheritDoc} */
     @Override public byte fieldsCount() {
-        return 14;
+        return nearExtras != null ? (byte)15 : (byte)9;
     }
 
     /** {@inheritDoc} */
     @Override public String toString() {
         return S.toString(GridNearAtomicSingleUpdateResponse.class, this, "parent");
+    }
+
+    /**
+     * Extra data for near nodes.
+     */
+    private static class NearExtras {
+        /** Indexes of keys for which values were generated on primary node (used if originating node has near cache). */
+        private boolean valsIdx;
+
+        /** Indexes of keys for which update was skipped (used if originating node has near cache). */
+        private boolean skipIdx;
+
+        /** Values generated on primary node which should be put to originating node's near cache. */
+        @GridToStringInclude
+        private CacheObject val;
+
+        /** Version generated on primary node to be used for originating node's near cache update. */
+        private GridCacheVersion ver;
+
+        /** Near TTLs. */
+        private long ttl = -1L;
+
+        /** Near expire times. */
+        private long expireTime = -1L;
     }
 }
